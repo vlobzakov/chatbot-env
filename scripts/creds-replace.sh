eb16f252546ea01f8c498d9e21682a591a8f9fbe
vlobzakov <lwo@ravatar.com>
2024-04-13 17:28:33 +0300

    Readme and manifest updated

--- scripts/creds-replace.sh
+++ scripts/creds-replace.sh
@@ -1,3 +1,9 @@
+a='password: "$2b$08$C8I.EZCHxPsJlaoOcZjKzupjNoN5cRSavs9H.T1by8iQVScu7f/cO",'
+b=`echo $1 | node-red admin hash-pw | awk -F\  '{print $2}'`
+c="password: \"$b\","
+
+sed -i "s|$a|$c|g" /root/creds-replace.js
+
 lead='^.*\/\/adminAuth: {'
 tail='^.*\/\*\* The following property can be used to enable HTTPS'
 sed -e "/$lead/,/$tail/{ /$lead/{p; r /root/creds-replace.js
